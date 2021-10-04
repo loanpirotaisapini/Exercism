f0a976663296a52e71d0cf1db5a5ed8e734c8b18
Loan PIROTAIS <loan.pirotais@apini.fr>
2021-10-04 16:05:31 +0200

    DEV: Commit après passage de tous les tests à TRUE

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,27 +1,39 @@
 export function hey(message: string): string {
 
-  var result: string = "";
+  var result: string = '';
   
-  // Si tout le message est en majuscule
-  if(isUpperCase(message)) {
-    // Dans le cas où le message est une question
-    if(message.indexOf("?") !== null) {
-      result = "Calm down, I know what I'm doing!";
-    } else { // Dans le cas contraire
-      result = "Whoa, chill out!";
+  // First, we have to verify if the message's a question
+  if(isQuestion(message.trim())) {
+    if (isUpperCase(message) && /[A-z]/.test(message)) { // In case it's in upper case
+      result = 'Calm down, I know what I\'m doing!';
+    } else { // Otherwise
+      result = 'Sure.';
     }
-  } else {
-    if(message.indexOf("?") !== null) {
-      result = "Sure.";
-    } else {
-      result = "Whatever.";
-    }
+  } else if(isUpperCase(message) && /[A-z]/.test(message)) { // If the message's in upper case and isn't a question
+    result = 'Whoa, chill out!';
+  } else if (message.trim() === '') { // If the message's empty
+    result = 'Fine. Be that way!';
+  } else { // Otherwise
+    result = 'Whatever.';
   }
 
   return result;
 }
 
-// Permet de vérifier si tout le message est en majuscule
+/**
+ * Verify if the string's in upper case
+ * @param str : String to verify
+ * @returns : true or false
+ */
 function isUpperCase(str: string): boolean {
   return str === str.toUpperCase() ? true : false;
+}
+
+/**
+ * Verify if the string's a question
+ * @param str : String to verify
+ * @returns : true or false
+ */
+function isQuestion(str: string): boolean {
+  return str.charAt(str.length - 1) === '?' ? true : false;
 }
