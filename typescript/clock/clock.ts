a74c208229d20627e68661229ff1685bdf47ab5f
Loan Pirotais <loan.pirotais@apini.fr>
2021-10-05 10:57:12 +0200

    DEV: Resolved clock exercism

--- typescript/clock/clock.ts
+++ typescript/clock/clock.ts
@@ -1,21 +1,62 @@
 export class Clock {
-  constructor(hour: unknown, minute?: unknown) {
-    throw new Error('Remove this statement and implement this function')
+
+  // Definition of class attributes
+  private hours: number;
+  private minutes: number;
+  private clock: Date;
+
+  constructor(hours: number, minutes: number = 0) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.clock = new Date();
+    this.clock.setHours(hours, minutes);
   }
 
-  public toString(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  /**
+   * Format the string display for the clock
+   */
+  public toString(): string {
+    
+    let hours: string = this.clock.getHours().toString();
+    let minutes: string = this.clock.getMinutes().toString();
+
+    // Format hours and minutes if on one digit
+    if (hours.length === 1) {
+      hours = '0' + hours;
+    }
+    if (minutes.length === 1) {
+      minutes = '0' + minutes;
+    }
+
+    return hours + ':' + minutes;
   }
 
-  public plus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  /**
+   * Permits to add some minutes to the clock
+   * @param minutesToAdd Minutes to add to the current clock
+   * @returns The new clock
+   */
+  public plus(minutesToAdd: number): string {
+    this.clock.setMinutes(this.minutes + minutesToAdd);
+    return this.toString();
   }
 
-  public minus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  /**
+   * Permits to remove some minutes to the clock
+   * @param minutesToRemove Minutes to remove to the current clock
+   * @returns The new clock
+   */
+  public minus(minutesToRemove: number): string {
+    this.clock.setMinutes(this.minutes - minutesToRemove);
+    return this.toString();
   }
 
-  public equals(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  /**
+   * Compare if two clocks are equals
+   * @param clockToCompare Clock given to compare
+   * @returns True if equals, otherwise false
+   */
+  public equals(clockToCompare: Clock): boolean {
+    return this.toString() === clockToCompare.toString();
   }
 }
