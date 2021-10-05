aa389c574a7714ee516ada870cccd72f1abad575
Julien Boucly <julien.boucly@apini.fr>
2021-10-05 15:23:42 +0200

    refactor: Opti

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,5 +1,4 @@
 export function count(sentence: string): Map<string, number> {
-  
   const regex = /[^\s]+/g;
   const wordCount = new Map();
   const words = sentence.toLowerCase().match(regex);
@@ -7,16 +6,11 @@ export function count(sentence: string): Map<string, number> {
   // If there's no word that matches the regex
   if (!words || words.length === 0) return wordCount;
 
-  // For each word
-  for (const word of words) {
-    // If the word already exists in our map
-    if (wordCount.get(word)) {
-      // We increase the number of ocurrences in the Map
-      wordCount.set(word, wordCount.get(word) + 1);
-    } else { // Otherwise, we add the word in the Map
-      wordCount.set(word, 1);
-    }
-  }
-  
+  words.forEach((word) =>
+    wordCount.get(word)
+      ? wordCount.set(word, wordCount.get(word) + 1)
+      : wordCount.set(word, 1)
+  );
+
   return wordCount;
 }
