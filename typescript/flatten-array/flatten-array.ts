675a34a9f302e6631c38d9a4b356ca8c330dbf66
Loan Pirotais <loan.pirotais@apini.fr>
2021-10-05 14:14:47 +0200

    DEV: Resolved flatten-array exercism

--- typescript/flatten-array/flatten-array.ts
+++ typescript/flatten-array/flatten-array.ts
@@ -1,3 +1,15 @@
-export function flatten(/* Parameters go here */) {
-  throw new Error('Remove this statement and implement this function')
+export function flatten(arrayToFlatten: any[], result: any[] = []): number[] {
+
+  // Foreach value of the given array
+  arrayToFlatten.forEach((value) => {
+    // If the value's an array
+    if (value instanceof Array) {
+      value = value.splice(0, value.length);
+      flatten(value, result);
+    } else if (value !== undefined && value !== null && result) {
+      result.push(value);
+    } 
+  });
+  
+  return result ? result : [];
 }
