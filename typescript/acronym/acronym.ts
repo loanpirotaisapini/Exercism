6ac1ec0ca781c6206fdfec9a160d8cb9751fea8e
Loan Pirotais <loan.pirotais@apini.fr>
2021-10-05 09:52:19 +0200

    DEV: Changes after verifications w/ Julien

--- typescript/acronym/acronym.ts
+++ typescript/acronym/acronym.ts
@@ -6,7 +6,8 @@ export function parse(phrase: string): string {
   var mot = phrase.match(REGEX);
 
   if(mot !== null) {
-    result = mot.reduce((acronym, char) => acronym += char[0].toUpperCase(), '');
+    // L'écriture `${acronym}${char[0]}` permet d'éviter les écriture de type acronym += char[0]
+    result = mot.reduce((acronym, char) => `${acronym}${char[0].toUpperCase()}`, '');
   }
 
   return result;
