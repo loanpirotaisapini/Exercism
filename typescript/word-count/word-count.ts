868e4507676f69580eb02d1d0562a8e51109b538
Loan Pirotais <loan.pirotais@apini.fr>
2021-10-05 10:32:01 +0200

    DEV: Resolved word-count exercism

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,3 +1,22 @@
-export function count() {
-  throw new Error('Remove this statement and implement this function')
+export function count(sentence: string): Map<string, number> {
+  
+  const regex = /[^\s]+/g;
+  const wordCount = new Map();
+  const words = sentence.toLowerCase().match(regex);
+
+  // If there's no word that matches the regex
+  if (!words || words.length === 0) return wordCount;
+
+  // For each word
+  for (const word of words) {
+    // If the word already exists in our map
+    if (wordCount.get(word)) {
+      // We increase the number of ocurrences in the Map
+      wordCount.set(word, wordCount.get(word) + 1);
+    } else { // Otherwise, we add the word in the Map
+      wordCount.set(word, 1);
+    }
+  }
+  
+  return wordCount;
 }
