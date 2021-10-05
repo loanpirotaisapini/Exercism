1cc388622d3fd0004763b61c2e9760485db9f34e
Loan Pirotais <loan.pirotais@apini.fr>
2021-10-05 17:51:15 +0200

    WIP: end of day 05/10/2021

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -1,13 +1,46 @@
 export class GradeSchool {
-  roster() {
-    throw new Error('Remove this statement and implement this function')
+
+  private school: SchoolMap = new SchoolMap();
+
+  roster(): { [key: number]: string[] } {
+
+    let schoolMap: SchoolMap = new SchoolMap();
+
+    let result: { [key: number]: string[] } = {};
+
+    this.school.forEach((value: string[], key: number) => {
+      schoolMap.set(key, value);
+    });
+
+    schoolMap.forEach((value: string[], key: number) => {
+      Object.assign(result, {
+        [key]: value
+      });
+    });
+
+    return result;
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function')
+  add(studentName: string, schoolGrade: number): void {
+    const students = this.school.get(schoolGrade) || [];
+
+    students.push(studentName);
+    this.school.set(schoolGrade, students.sort());
+
   }
 
-  grade() {
-    throw new Error('Remove this statement and implement this function')
+  grade(searchedGrade: number): string[] {
+    return (typeof this.school.get(searchedGrade) === 'undefined') ? [] : this.school.get(searchedGrade) as string[];
   }
+}
+
+
+export class SchoolMap extends Map<number, string[]> {
+  
+  public get(key: number): string[] {
+
+    const result = super.get(key) || [];
+    return [...result];
+  }
+
 }
